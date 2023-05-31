597f56a4ab1f493e1a49944bbb4c19d8edc46fde
matibf99 <matibf99@gmail.com>
2023-05-31 19:19:24 -0300

    mongo configuration

--- src/main/java/com/example/microserviceuab/config/MongoConfig.java
+++ src/main/java/com/example/microserviceuab/config/MongoConfig.java
@@ -1,46 +1,31 @@
 package com.example.microserviceuab.config;
 
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.lang.NonNull;
-import org.bson.UuidRepresentation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 
-@EnableMongoRepositories("com.example.microserviceuab.repository")
-public class MongoConfig extends AbstractMongoClientConfiguration {
+@Configuration
+public class MongoConfig {
+    private final String uri;
 
-    private final String url;
-    private final String database;
-
-    public MongoConfig(@Value("${spring.data.mongodb.url}") String url,
-                       @Value("${spring.data.mongodb.database}") String database) {
-        this.url = url;
-        this.database = database;
+    @Autowired
+    public MongoConfig(@Value("${spring.data.mongodb.uri}") String uri) {
+        this.uri = uri;
     }
 
-    @NonNull
-    @Override
-    protected String getDatabaseName() {
-        return database;
-    }
-
-    @NonNull
     @Bean
-    public MongoClient mongoClient() {
-        ConnectionString connectionString = new ConnectionString(url);
+    public MongoDatabaseFactory mongoDbFactory() {
+        return new SimpleMongoClientDatabaseFactory(uri) {
 
-        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
-                .applyConnectionString(connectionString)
-                .uuidRepresentation(UuidRepresentation.STANDARD)
-                .applyConnectionString(connectionString)
-                .build();
-
-        return MongoClients.create(mongoClientSettings);
+        };
     }
 
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoDbFactory());
+    }
 }
