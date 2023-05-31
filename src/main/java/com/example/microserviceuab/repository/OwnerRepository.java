963d04ffba7bb952454af19087ff79e8ed6d493c
matibf99 <matibf99@gmail.com>
2023-05-31 19:21:44 -0300

    fixed query in OwnerRepository.java

--- src/main/java/com/example/microserviceuab/repository/OwnerRepository.java
+++ src/main/java/com/example/microserviceuab/repository/OwnerRepository.java
@@ -3,6 +3,7 @@ package com.example.microserviceuab.repository;
 import com.example.microserviceuab.domain.Owner;
 import org.springframework.data.mongodb.repository.ExistsQuery;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,7 +11,10 @@ import java.util.Optional;
 @Repository
 public interface OwnerRepository extends MongoRepository<Owner, String> {
     Optional<Owner> findByUsername(String username);
+
+    @Query("{ 'chatId':  ?0 }")
     Optional<Owner> findByChatId(Long chatId);
+
     @ExistsQuery("{ 'username':  ?0 }")
     boolean existsByUsername(String username);
 }
