6f7fac7b980b969b462e6a9449aac80075a20e75
Chaza07 <Chazita9@gmail.com>
2023-06-25 16:21:52 -0300

    Test Owner and Client

--- src/main/java/com/example/microserviceuab/domain/Room.java
+++ src/main/java/com/example/microserviceuab/domain/Room.java
@@ -1,7 +1,9 @@
 package com.example.microserviceuab.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
 import org.springframework.data.mongodb.core.mapping.FieldType;
@@ -9,6 +11,8 @@ import org.springframework.data.mongodb.core.mapping.MongoId;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Document(collection = "room")
 public class Room {
     @MongoId(FieldType.OBJECT_ID)
